f2571b0f21215153c38097d1f99067a73c932009
javalerop87 <67809975+javalerop87@users.noreply.github.com>
2024-07-11 11:00:48 -0600

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello, world');
+  res.send('hello, world 2 de prueba');
 });
 
 app.listen(process.env.PORT || 8080);
